6fce81af414d2806771cfbbabfdeace3c53ee3c7
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-11 18:46:08 +0530

    Use case 4

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -10,47 +10,44 @@ package com.bridgelabz;
 public class EmpWage {
     int WAGE_PER_HR = 20 ;
     /**
-     * @return randhrs() gives out a number between 1 and 8
+     *  function checkWorkCase gives out if the employee is part timer or full timer using  switch case
      */
-    public int randhrs() {
-        int emp_hrs;
-        emp_hrs = (int) Math.floor((Math.random() * 10 % 8)+1);
-        if(emp_hrs<=4)
-        {
-            System.out.println("Employee Hours:"+emp_hrs);
-            System.out.println("Employee is a Part Time Worker");
-            return emp_hrs;
+    public void checkWorkCase() {
+        int emp_hrs=0 ;
+        int emp_check;
+        emp_check = (int) (Math.random() * 10) % 3;
+
+        int time = emp_check;
+        switch (time) {
+            case 2:
+                System.out.println("Employee is a full time worker");
+                emp_hrs = 8;
+                break;
+            case 1:
+                System.out.println("Employee is a part time worker");
+                emp_hrs = 4;
+                break;
+            case 0:
+                System.out.println("Employee data unavailable ");
+                emp_hrs = 0;
+                break;
         }
-        else{
-            System.out.println("Employee Hours:"+emp_hrs);
-            System.out.println("Employee is a Full Timer Worker");
-            return emp_hrs;
-        }
+
     }
 
-    /**
-     * @param emp_hrs gives the total working hours of and employee
-     *
-     *  totpay() gives out total pay
-     */
 
-
-    public void totpay(int emp_hrs) {
-        int totalpay= WAGE_PER_HR * emp_hrs;
-        System.out.println("Employees total pay for the days is :"+totalpay);
-    }
 
     public static void main(String[] args) {
-        System.out.println("Program to get total pay of the employee ");
-        System.out.println("_______________________________________________________");
+        System.out.println("Program to give out if the employee is a part timer or full timer ");
+        System.out.println("__________________________________________________________________");
 /**
  * Using methods
- * we use the methods created in class EmpWageUC2 by making its object
+ * we use the methods created in class EmpWageCheck by making its object
  */
         EmpWage emp = new EmpWage();
-        int emp_hrs;
-        emp_hrs= emp.randhrs();
-        emp.totpay(emp_hrs);
+
+        emp.checkWorkCase();
+
 
     }
 
