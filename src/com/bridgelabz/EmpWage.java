a5fdfb5dc5db75cc633d46cf54c2807eb8b80096
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-11 18:14:27 +0530

    Use Case 2 To calculate employee wage based on hours

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,48 +1,47 @@
 package com.bridgelabz;
 
 /**
- * Program using OOPS concept to check if the employee is present or absent
+ * Program using OOPS concept to check employee wage based on hours
  *
  * @author Shubham
  * @version 1.0
  * @since 10/08/2021
  */
 public class EmpWage {
+    int WAGE_PER_HR = 20 ;
     /**
-     * @return rand() gives out a number between 0 and 1
+     * @return randhrs() gives out a number between 1 and 8
      */
-    public int rand() {
-        int emp_check;
-        emp_check = (int) Math.floor(Math.random() * 10 % 2);
-        return emp_check;
+    public int randhrs() {
+        int emp_hrs;
+        emp_hrs = (int) Math.floor((Math.random() * 10 % 8)+1);
+        return emp_hrs;
     }
 
     /**
-     * @param emp_check is compared to 1
-     *                  if emp_check is equal to 1 then the employee is present
-     *                  else the employee is present i.e emp_check is equal to 0
-     *                  ifpresent() gives out if the employee is present or absent
+     * @param emp_hrs gives the total working hours of and employee
+     *
+     *  totpay() gives out total pay
      */
-    public void ifpresent(int emp_check) {
-        if (emp_check == 1) {
-            System.out.println("Employee is Present");
-        } else
-            System.out.println("Employee is Absent");
+    public void totpay(int emp_hrs) {
+        int totalpay= WAGE_PER_HR * emp_hrs;
+        System.out.println("Employees total pay for the days is :"+totalpay);
     }
 
     public static void main(String[] args) {
-        System.out.println("Program to check if the employee is present or absent");
         System.out.println("Program to get total pay of the employee ");
         System.out.println("_______________________________________________________");
 /**
  * Using methods
- * we use the methods created in class EmpWage by making its object
+ * we use the methods created in class EmpWageUC2 by making its object
  */
         EmpWage emp = new EmpWage();
-        int pres;
-        pres = emp.rand();
-        emp.ifpresent(pres);
+        int emp_hrs;
+        emp_hrs= emp.randhrs();
+        System.out.println("Total Hours:"+emp_hrs);
+        emp.totpay(emp_hrs);
 
     }
 
 }
+
